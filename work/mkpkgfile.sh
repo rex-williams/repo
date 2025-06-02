b6ec526b65b1f614f99ed1d0462c95b3c83465fa
Rex Williams <degenincel070@proton.me>
2025-06-01 21:07:50 -0500

    09:07PM

--- work/mkpkgfile.sh
+++ work/mkpkgfile.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 package="$(echo "$1" | sed 's_.*/__g')"
-mkdir $package
-echo "name='$package'" > $package/Pkgfile
+echo "name='$package'" > Pkgfile
 version="$(cat $1/version | cut -d' ' -f1)"
-echo "version='$version'" >> $package/Pkgfile
+echo "version='$version'" >> Pkgfile
 sources="$(cat $1/sources | sed 1q | sed "s/$version/\${version}/g")"
-echo "sources=\"$sources\"" >> $package/Pkgfile
+echo "sources=\"$sources\"" >> Pkgfile
 if [ -f "$1/depends" ]; then
     depends="$(cat $1/depends | uniq -u | tr '\n' ' ')"
-    echo "depends='$depends'" >> $package/Pkgfile
+    echo "depends='$depends'" >> Pkgfile
 fi
-echo 'build() {' >> $package/Pkgfile
-cat "$1/build" | sed 's_#!/bin/sh -e__g' | sed 's/$1/$INSTALL/g' >> $package/Pkgfile
-echo '}' >> $package/Pkgfile
+echo 'build() {' >> Pkgfile
+cat "$1/build" | sed 's_#!/bin/sh -e__g' | sed 's/$1/$INSTALL/g' >> Pkgfile
+echo '}' >> Pkgfile
 rm -rfv "$1"
+mkdir "$package"
+mv Pkgfile "$package"

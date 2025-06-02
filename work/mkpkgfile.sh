e7f477b9f46437cf3408f2cae84103891ed3dd4c
Rex Williams <degenincel070@proton.me>
2025-06-01 20:57:07 -0500

    08:57PM

--- work/mkpkgfile.sh
+++ work/mkpkgfile.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 package="$(echo "$1" | sed 's_.*/__g')"
-echo "name='$package'" > $package.pkg
+mkdir $package
+echo "name='$package'" > $package/Pkgfile
 version="$(cat $1/version | cut -d' ' -f1)"
-echo "version='$version'" >> $package.pkg
+echo "version='$version'" >> $package/Pkgfile
 sources="$(cat $1/sources | sed 1q | sed "s/$version/\${version}/g")"
-echo "sources=\"$sources\"" >> $package.pkg
+echo "sources=\"$sources\"" >> $package/Pkgfile
 if [ -f "$1/depends" ]; then
     depends="$(cat $1/depends | uniq -u | tr '\n' ' ')"
-    echo "depends='$depends'" >> $package.pkg
+    echo "depends='$depends'" >> $package/Pkgfile
 fi
-echo 'build() {' >> $package.pkg
-cat "$1/build" | sed 's_#!/bin/sh -e__g' | sed 's/$1/$INSTALL/g' >> $package.pkg
-echo '}' >> $package.pkg
+echo 'build() {' >> $package/Pkgfile
+cat "$1/build" | sed 's_#!/bin/sh -e__g' | sed 's/$1/$INSTALL/g' >> $package/Pkgfile
+echo '}' >> $package/Pkgfile
 rm -rfv "$1"
